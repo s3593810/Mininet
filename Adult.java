45341df75910a8efd2a2333736d0884198487af5
Md Shakil Khan <31284207+RMIT-s3593810-MDSHAKIL-KHAN@users.noreply.github.com>
2018-03-19 20:40:29 -0700

    Add files via upload

---
+++ Adult.java
@@ -0,0 +1,27 @@
+package Assignment1;
+
+public class Adult extends User{
+
+	public Adult(String N, int A, String G, String P, String S) {
+		super(N, A, G, P, S);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Adult (String N, int A, String G, String P) {
+		super(N, A, G, P);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Adult(String N, int A, String G) {
+		super(N, A, G);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void AddFriend() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+}
---
+++ Test.java
@@ -0,0 +1,13 @@
+package Assignment1;
+
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		User [] Input=new User[200];
+		Input[0]=new Adult("Shakil", 25, "Male");
+		Input[1]=new Adult("Ali", 26, "Male", "AliPicture", "RMIT Student");
+	}
+
+}
