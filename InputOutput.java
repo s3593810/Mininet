8808909ac8140041d38513191d28572fde57ee28
Md Shakil Khan <31284207+RMIT-s3593810-MDSHAKIL-KHAN@users.noreply.github.com>
2018-03-28 11:05:02 +1100

    This Version tries incorporating Reading and Writing From Text files

---
+++ InputOutput.java
@@ -0,0 +1,41 @@
+
+
+package socialNetwork;
+//
+//	import java.io.BufferedReader;
+//	import java.io.FileReader;
+//	import java.io.FileWriter;
+//	import java.io.IOException;
+//	import java.io.PrintWriter;
+//
+	public class InputOutput {
+//		public void Writer() {
+//			try {
+//				FileWriter Input=new FileWriter("User.txt");
+//				PrintWriter Print= new PrintWriter(Input);
+//				
+//				
+//				Print.println(list);
+//				Print.close();
+//			}
+//			catch(IOException e) {
+//				System.out.println("Error!!");
+//				
+//			}
+//		}
+//		public void Reader() {
+//			try {
+//				FileReader Read = new FileReader("User.txt");
+//				BufferedReader Reader=new BufferedReader(Read);
+//				String S;
+//				while ((S=Reader.readLine()) !=null) {
+//					System.out.println(S+"\n");
+//				}
+//				Reader.close();
+//			}
+//			catch(IOException e) {
+//				System.out.println("File Not Found!!");
+//			}
+//		}
+	}
+
